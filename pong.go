98c7756cc73cbbc06233982f19f7fbbe8b78d7ab
Omar Khawaja <khawaja910@gmail.com>
2020-08-01 13:02:51 -0400

    add pong handler

---
+++ pong.go
@@ -0,0 +1,20 @@
+package hello
+
+func (c *Client) PongOrder() error {
+	r, err := c.newRequest("GET", "/pong")
+	if err != nil {
+		return err
+	}
+
+	req, err := r.toHTTP()
+	if err != nil {
+		return err
+	}
+
+	_, err = c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
